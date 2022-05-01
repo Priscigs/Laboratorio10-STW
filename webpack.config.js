ae36ad0aec42489380d446e9b2f634033b80d966
Priscigs <fatimag811@gmail.com>
2022-05-01 12:57:44 -0600

    ⚙️ Main Structure

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const FileLoader = require('file-loader');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'bundle_react')
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: '[name].css'
+        }),
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, 'css-loader'],
+            },
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react'] 
+                    }
+                }
+            },
+            {
+                test: /.(png|jpe?g|gif|mp4|mp3|webp|svg)$/i,
+                type: 'asset/resource',
+                generator: {
+                    filename:'[name][ext]',
+                    outputPath:'assets/',
+                    publicPath:'assets/'
+                }
+            }
+        ],
+    },
+};

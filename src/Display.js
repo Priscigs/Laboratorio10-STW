09a7242b1e2257671c9090ace118be743c2e1da4
Priscigs <fatimag811@gmail.com>
2022-05-05 19:17:51 -0600

    ➕➕➕🗂 Design Changes

--- src/Display.js
+++ src/Display.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './Display.css';
+import Calculator from './Calculator';
 
 class Display extends React.Component {
     render() {
         return (
             <div className='display'>
-                
+                {/* {entryN} */}
             </div>
         );
     }

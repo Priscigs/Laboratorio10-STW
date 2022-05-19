d0a217c36473a7ff7f702948f62b4588f1e888b2
Priscigs <fatimag811@gmail.com>
2022-05-19 16:42:44 -0600

    🗂 File Changes

---
+++ src/Header.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import './Header.scss';
+
+function Header() {
+  return (<div className="container"><h1 className="words">CALCULADORA</h1></div>);
+}
+export default Header;
---
+++ src/StartApp.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './StartApp.scss';
+import Header from './Header';
+import Display from './Display';
+
+function StartApp() {
+  return (
+    <>
+      <Header />
+      <Display />
+    </>
+  );
+}
+
+export default StartApp;

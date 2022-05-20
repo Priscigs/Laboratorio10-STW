a76fedb00051e009d61a8bf7fb6b6b1b7553c017
Priscigs <fatimag811@gmail.com>
2022-05-19 18:55:52 -0600

    ⚙️ Configuration Changes

--- jest.config.js
+++ jest.config.js
@@ -7,6 +7,8 @@ module.exports = {
 
   transformIgnorePatterns: ['[/\\\\]node_modules[/\\\\].+\\.(js|jsx|mjs)$'],
 
+  moduleNameMapper: { '^.+\\.(css|less|scss)$': 'babel-jest' },
+
   // transform: {
   //   "^.+\\.(js|jsx|mjs)$": '<rootDir>/src/setupTests.js'
   // },

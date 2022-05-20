9d285b8f5c2a9d916a784e19ec11a4d7fdfb2363
Priscigs <fatimag811@gmail.com>
2022-05-19 21:17:47 -0600

    🗂 Test

--- src/_tests_/StartApp.test.js
+++ src/_tests_/StartApp.test.js
@@ -6,14 +6,12 @@ import { render, screen } from '@testing-library/react';
 import React from 'react';
 import StartApp from '../StartApp';
 
-describe("Testing Component StartApp", () => {
-
-    // TEST DENTRO DEL COMPONENTE DESCRITO PREVIAMENTE
-    it("Testing Component StartApp", () => {
-        render(<StartApp></StartApp>)
-
-        // BUSCAR ELEMENTO EN DOM CON ID ESPECÍFICO
-        const testElement = screen.getByTestId('input-text-test');
-        expect(testElement).toBeInTheDocument();
-    });
+describe('Testing Component StartApp', () => {
+  // TEST DENTRO DEL COMPONENTE DESCRITO PREVIAMENTE
+  it('Testing Component StartApp', () => {
+    render(<StartApp />);
+    // BUSCAR ELEMENTO EN DOM CON ID ESPECÍFICO
+    const testElement = screen.getByTestId('input-text-test');
+    expect(testElement).toBeInTheDocument();
+  });
 });
